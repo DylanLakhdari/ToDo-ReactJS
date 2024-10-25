9234ac613c8e3a8d4eb9270eb62ca74dafa55ff7
Dylan Lakhdari <emmelf.exe@gmail.com>
2024-10-25 14:09:53 +0200

    Ajout des methodes

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,71 @@ import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import ListGroup from "react-bootstrap/ListGroup";
 
-function App() {
-  return (
-    <>
-      <div>
-        <p>Hello world</p>
-      </div>
-    </>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    //Stockage des éléments de la todo
+    this.state = {
+      userInput: "",
+      list: [],
+    };
+  }
+
+  //Mise à jour de l'input de l'user
+  updateInput(value) {
+    this.setState({
+      userInput: value,
+    });
+  }
+
+  addItem() {
+    if (this.state.userInput == !"") {
+      const userInput = {
+        //Ajoute un id unique pour la suppression
+        id: Math.random(),
+
+        //Ajoute la value de l'utilisateur dans la liste
+        value: this.state.userInput,
+      };
+
+      // Met à jour la liste
+      const list = [...this.state.list];
+      list.push(userInput);
+
+      //Réinitialise l'input
+      this.setState({
+        list,
+        userInput: "",
+      });
+    }
+  }
+
+  deleteItem(key) {
+    const list = [...this.state.list];
+
+    //Filtre la liste pour retirer l'élément avec son id
+    const updateList = list.filter((item) => item.id !== key);
+
+    //Met à jour la liste dans state
+    this.setState({
+      list: updateList,
+    });
+  }
+
+  editItem = (index) => {
+    const todos = [...this.state.list];
+
+    //Demande à l'utilisateur une valeur avec un prompt
+    const editedTodo = prompt("Edit the todo:");
+
+    //Si la nouvelle valeur n'est ni vide ni "null" met à jour l'élément
+    if (editedTodo !== null && editedTodo.trim() !== "") {
+      let updatedTodos = [...todos];
+      updatedTodos[index].value = editedTodo;
+      this.setState({
+        list: updatedTodos,
+      });
+    }
+  };
+}
